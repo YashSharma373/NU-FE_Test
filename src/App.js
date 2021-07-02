4dd8ba67a9cb8efec06ac6f8145b2b09fb65e422
Yash Sharma <55599815+YashSharma373@users.noreply.github.com>
2021-07-02 20:55:32 +0530

    Created Drawer Drawer Added which render some extra info for selected table row

--- src/App.js
+++ src/App.js
@@ -10,17 +10,42 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import axios from "axios";
 import "./App.css";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import CloseIcon from "@material-ui/icons/Close";
 
 const countriesURL = "https://restcountries.eu/rest/v2/all";
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650,
+    minWidth: 650
   },
+  drawerWidh: {
+    width: "250px"
+    // backgroundColor : "green",
+    // height : '100%'
+  },
+  closeIcon: {
+    cursor: "pointer"
+  }
 });
 
 function App() {
   const [countriesData, setCountriesData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [list, setList] = useState({
+    name: "",
+    capital: "",
+    img: "",
+    population: "",
+    region: "",
+    languages: [],
+    currencies: [],
+    nativeName: ""
+  });
+
   const classes = useStyles();
 
   const getCountriesWithAxios = async () => {
@@ -32,6 +57,21 @@ function App() {
   useEffect(() => {
     getCountriesWithAxios();
   }, []);
+
+  function openDrawer(e) {
+    console.log(e);
+    setList({
+      name: e.name,
+      capital: e.capital,
+      img: e.flag,
+      population: e.population,
+      region: e.region,
+      languages: e.languages,
+      currencies: e.currencies,
+      nativeName: e.nativeName
+    });
+    setOpen(true);
+  }
 
   return (
     <>
@@ -60,14 +100,19 @@ function App() {
               </TableHead>
               <TableBody>
                 {countriesData.map((country) => (
-                  <TableRow>
+                  <TableRow
+                    classes={{
+                      root: classes.closeIcon
+                    }}
+                    onClick={() => openDrawer(country)}
+                  >
                     <TableCell component="th" scope="row">
-                      {country.capital}
+                      {country.name}
                     </TableCell>
                     <TableCell align="right">
-                      <img src={country.flags} alt="" width="32px" />
+                      <img src={country.flag} alt="" width="32px" />
                     </TableCell>
-                    <TableCell align="right">{country.name}</TableCell>
+                    <TableCell align="right">{country.capital}</TableCell>
                     <TableCell align="right">{country.population}</TableCell>
                     <TableCell align="right">{country.region}</TableCell>
                   </TableRow>
@@ -75,6 +120,52 @@ function App() {
               </TableBody>
             </Table>
           </TableContainer>
+          <Drawer anchor="right" open={open} onClose={() => setOpen(false)}>
+            <CloseIcon
+              classes={{
+                root: classes.closeIcon
+              }}
+              onClick={() => setOpen(false)}
+            />
+            <List className={classes.drawerWidh}>
+              <ListItem>
+                {" "}
+                <img src={list.img} alt="flag" height="70px" />
+              </ListItem>
+              <Divider />
+              <ListItem>{"Name : " + list.name}</ListItem>
+              <ListItem> {"Capital : " + list.capital} </ListItem>
+              <ListItem> {"Region : " + list.region} </ListItem>
+              <ListItem> {"Population : " + list.population} </ListItem>
+              <ListItem>{"Native Name : " + list.nativeName}</ListItem>
+              <Divider />
+              <ListItem>
+                {
+                  <>
+                    <p>{"Languages"}</p>
+                    <ul>
+                      {list.languages.map((lang) => (
+                        <li>{lang.name}</li>
+                      ))}
+                    </ul>
+                  </>
+                }
+              </ListItem>
+              <Divider />
+              <ListItem>
+                {
+                  <>
+                    <p>{"Currencies"}</p>
+                    <ul>
+                      {list.currencies.map((curr) => (
+                        <li>{curr.name}</li>
+                      ))}
+                    </ul>
+                  </>
+                }
+              </ListItem>
+            </List>
+          </Drawer>
         </Grid>
       </Grid>
     </>
